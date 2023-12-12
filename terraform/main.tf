be26e4211204beda60ca3162df9e7bd7843e4e38
Mário Abreu <52406590+mracfa@users.noreply.github.com>
2023-12-12 11:43:02 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,39 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 0.12"
+
+  backend "azurerm" {
+    subscription_id      = "b8ee75a4-9052-406d-a746-d25384000b5f"
+    resource_group_name  = "mario-tf-state"
+    storage_account_name = "mariotfstate"
+    container_name       = "tfstatefiles"
+    key                  = "mario.dev.tfstate"
+  }  
 }
 
-variable "subject" {
-   type = string
-   default = "tfctl-rc"
-   description = "Subject to hello"
+provider "azurerm" {
+  subscription_id = "b8ee75a4-9052-406d-a746-d25384000b5f"
+  features {}
 }
 
-output "hello_world" {
-  value = "hey hey ya, ${var.subject}!"
+
+
+variable "tags" {
+  type        = map(string)
+  default     = {
+    Product: "Store"
+    AppGroup: "Store"
+    Environment: "Development"
+    Team: "DevopsSAndA"
+    Customer: "Relex"
+    CostCenter: "ResearchAndDevelopment"
+    ManagedBy: "Terraform"
+    ManagedIn: "mario-terraform"
+  }
+}
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "mario-rg2"
+  location = "West Europe"
+  tags = var.tags 
 }
